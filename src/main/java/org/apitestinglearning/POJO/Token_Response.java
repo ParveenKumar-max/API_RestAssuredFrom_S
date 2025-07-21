e0a283af22387811cc623cda1d69d1cc3b179a35
ParveenKumar-max <Parveendogra2@gmail.com>
2025-07-22 00:32:25 +0530

    Rest Assured Project From Scratch -- Create POJO files

---
+++ src/main/java/org/apitestinglearning/POJO/Token_Response.java
@@ -0,0 +1,21 @@
+package org.apitestinglearning.POJO;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Token_Response {
+
+    @SerializedName("token")
+    @Expose
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+}
